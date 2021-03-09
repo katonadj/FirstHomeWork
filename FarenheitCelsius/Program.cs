446f99dc0656bc6e5fbac48bdb630dc152b261a8
katonadj <solider1987@gmail.com>
2021-03-09 09:20:53 +0100

    Add project files.

---
+++ FarenheitCelsius/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FarenheitCelsius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Farenheit-Celsius");
+            double celsius;
+            double fahrenheit;//kiszámoljuk
+            Console.Write("Farenheit = ");
+            fahrenheit = Convert.ToDouble(Console.ReadLine());//A Console ReadLine Szöveges típust olvas be
+            celsius = (fahrenheit -32) / 1.8;
+            Console.WriteLine($"{fahrenheit:F2} °F = {celsius:F2} °C");
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Nemprim/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Nemprim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Nem prímek
+            * Bekér a felhasználótól egy pozitív egész számot és kiírja az összes ennél kisebb olyan egész számot, amely nem prím
+            */
+            Console.WriteLine("Kérek egy pozitív egész számot:");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            for (int i = szam-1; i > 1; i--)
+            {
+                bool vanoszto = false;
+                for (int j = 2; j < i / 2; j++)//1 et és önmagat kihagyom
+                {
+                    if (i % j == 0)
+                    {
+                        vanoszto = true;
+                        break; //megszakítás, kilép a ciklusból
+                    }
+                }
+                if (vanoszto)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
